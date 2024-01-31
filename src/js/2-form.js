e0615ce2f2602623c429e3a55aeec83f5391a01c
Melnyk Roman <melnikroman2005@ukr.net>
2024-01-31 20:14:47 +0200

    'update'

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onBtnClick);
+
+function onFormInput() {
+  const inputValue = form.elements.email.value.trim();
+  const textareaValue = form.elements.message.value.trim();
+  const data = {
+    email: inputValue,
+    message: textareaValue,
+  };
+  saveToLS(STORAGE_KEY, data);
+}
+
+function onBtnClick(e) {
+  e.preventDefault();
+  console.log(loadFromLS(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+function getData() {
+  const { email, message } = loadFromLS(STORAGE_KEY) || {};
+  form.elements.email.value = email || '';
+  form.elements.message.value = message || '';
+}
+
+getData();
+
+function saveToLS(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadFromLS(key) {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (error) {
+    return error;
+  }
+}
